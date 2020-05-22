5b74a38ba3275a093d6bedfc39af44c7a2544d0e
Goh Sato <goh.sato@outlook.com>
2020-05-21 17:11:58 -0700

    Build tree

--- main.c
+++ main.c
@@ -2,28 +2,59 @@
 #include <stdlib.h>
 
 #define MAX_BUFFER_SIZE 1024
+#define CHARSET_SIZE 256
 
 // Heres what I had in mind, doesn't effect any code I've written so feel free to change
 struct Node
 {
     struct Node *left;
     struct Node *right;
-    int weight;
-    struct CharCount *c;
+    int count;
+    char c;
 };
 
-struct CharCount
+void enqueue(struct Node queue[], int *queue_length, struct Node node)
 {
-    char c;
-    int count;
-};
+    queue[*queue_length] = node;
+    (*queue_length)++;
+}
+
+struct Node dequeue(struct Node queue[], int *queue_length)
+{
+    int min = 0;
+    int i = 0;
+    for (i = 0; i < *queue_length; i++)
+    {
+        if (queue[i].count < queue[min].count)
+        {
+            min = i;
+        }
+    }
+    struct Node min_node = queue[min];
+
+    queue[min] = queue[*queue_length - 1];
+    (*queue_length)--;
+    return min_node;
+}
+
+void print_queue(struct Node queue[], int queue_length)
+{
+    printf("Q len %d\n", queue_length);
+
+    int i = 0;
+    while (i < queue_length)
+    {
+        printf("%c %d\n", queue[i].c, queue[i].count);
+        i++;
+    }
+}
 
 // used in conjunction with test_char_count.sh
 void test_char_count(int char_count[])
 {
     int i = 0;
     int total = 0;
-    while (i < 256)
+    while (i < CHARSET_SIZE)
     {
         if (char_count[i] != 0)
         {
@@ -54,7 +85,7 @@ int main(int argc, char **argv)
     char *f_name = argv[1];
 
     // index array via ascii of char, value is its count
-    int char_count[256] = {0};
+    int char_count[CHARSET_SIZE] = {0};
 
     FILE *f_ptr = fopen(f_name, "rb");
     if (f_ptr == NULL)
@@ -113,5 +144,37 @@ int main(int argc, char **argv)
 
     // test_char_count(char_count); // uncomment to test char count
 
-    // continue here...
+    // -------------------------------
+    // Create Priority Queue
+
+    struct Node priority_queue[CHARSET_SIZE];
+
+    int i = 0;
+    int queue_length = 0;
+    while (i < CHARSET_SIZE)
+    {
+        if (char_count[i] > 0)
+        {
+            priority_queue[queue_length].count = char_count[i];
+            priority_queue[queue_length].c = i;
+            queue_length++;
+        }
+        i++;
+    }
+
+    print_queue(priority_queue, queue_length);
+
+    while (queue_length > 1)
+    {
+        struct Node left_node = dequeue(priority_queue, &queue_length);
+        struct Node right_node = dequeue(priority_queue, &queue_length);
+        struct Node new_node;
+        new_node.count = left_node.count + right_node.count;
+        new_node.left = &left_node;
+        new_node.right = &right_node;
+        enqueue(priority_queue, &queue_length, new_node);
+    }
+
+    struct Node root = dequeue(priority_queue, &queue_length);
+    print_queue(priority_queue, queue_length);
 }
