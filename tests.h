136b81a319dc14d8e1f5df6ac65d305eaa307c5f
Isaac Cormack <isaac.m.cormack@gmail.com>
2020-07-09 11:06:21 -0700

    fix trailing white space at end of first line of encoding causing decoder to hang, get rid of many files for encoder with makefile

--- tests.h
+++
@@ -1,4 +0,0 @@
-void test_char_count(int char_count[]);
-void test_encoding_map(char *encoding[]);
-void print_expected_huffman_code(char *infile, char *encoding[]);
-void test_huffman_code(char *encoding[]);
