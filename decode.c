bc0004070ec594a1dea6f963d65011ad56af687a
Goh Sato <goh.sato@outlook.com>
2020-07-29 16:05:15 -0700

    Convert reference tables by int

--- decode.c
+++ decode.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-int MAX_CHAR_ENCODING_LEN = 64;
-
+#define MAX_CHAR_ENCODING_LEN 64
+#define MAX_NUM_TABLES 40
 typedef struct Entry
 {
     char c;
-    uint8_t code_length;
-    struct Entry **innertable;
+    uint8_t code_length_table_num;
 } Entry;
 
 void print_binary(uint64_t number, int start, int length)
@@ -23,12 +22,14 @@ void print_binary(uint64_t number, int start, int length)
     }
 }
 
-int add_table_entry(char c, uint64_t binary, uint8_t bin_length, Entry *table[])
+int add_table_entry(char c, uint64_t binary, uint8_t bin_length, uint8_t table_n, Entry ***all_tables, int *total_number_of_tables)
 {
     Entry *entry = (Entry *)malloc(sizeof(Entry));
 
     entry->c = c;
-    entry->code_length = bin_length;
+    entry->code_length_table_num = bin_length;
+
+    Entry **table = all_tables[table_n];
 
     unsigned char lower = binary >> (MAX_CHAR_ENCODING_LEN - 8);
     unsigned char upper = lower | (0b11111111 >> bin_length);
@@ -40,7 +41,7 @@ int add_table_entry(char c, uint64_t binary, uint8_t bin_length, Entry *table[])
         // If encoding length is <= 8, add entries
         if (bin_length <= 8)
         {
-            if (table[i] == NULL || table[i]->code_length < entry->code_length)
+            if (table[i] == NULL || table[i]->code_length_table_num < entry->code_length_table_num)
             {
                 table[i] = entry;
             }
@@ -53,14 +54,17 @@ int add_table_entry(char c, uint64_t binary, uint8_t bin_length, Entry *table[])
             {
                 // If there's no existing inner table, create one and fill.
                 Entry **inner_table = (Entry **)malloc(256 * sizeof(Entry *));
-                entry->innertable = inner_table;
-                add_table_entry(c, (binary << 8), bin_length - 8, inner_table);
+                entry->code_length_table_num = *total_number_of_tables;
+                all_tables[*total_number_of_tables] = inner_table;
+                *total_number_of_tables = *total_number_of_tables + 1;
+                add_table_entry(c, (binary << 8), bin_length - 8, entry->code_length_table_num, all_tables, total_number_of_tables);
+                entry->c = 0;
                 table[i] = entry;
             }
             else
             {
                 // If there is an existing inner table, put entry in that table.
-                add_table_entry(c, (binary << 8), bin_length - 8, table[i]->innertable);
+                add_table_entry(c, (binary << 8), bin_length - 8, table[i]->code_length_table_num, all_tables, total_number_of_tables);
             }
             // printf(" exit_inner ");
         }
@@ -68,13 +72,14 @@ int add_table_entry(char c, uint64_t binary, uint8_t bin_length, Entry *table[])
     return 0;
 }
 
-void build_lookup_table(Entry **root_table, FILE *input_file){
+void build_lookup_table(FILE *input_file, Entry ***all_tables, int *total_number_of_tables){
     int c = fgetc(input_file);
     if (feof(input_file))
     {
         fprintf(stderr, "File is empty. Exiting...\n");
         exit(1);
     }
+    Entry **root_table = all_tables[0];
     while (!feof(input_file))
     {
         uint8_t bin_length = 0;
@@ -97,7 +102,7 @@ void build_lookup_table(Entry **root_table, FILE *input_file){
         }
 
         // Add encoding mapping to table
-        add_table_entry(c, binary, bin_length, root_table);
+        add_table_entry(c, binary, bin_length, 0, all_tables, total_number_of_tables);
         // printf(" %c(%d)\n", c, c);
 
         if (binary_stream == '\n')
@@ -110,12 +115,13 @@ void build_lookup_table(Entry **root_table, FILE *input_file){
     }
 }
 
-void decode_stream(Entry **root_table, FILE *input_file){
+void decode_stream(Entry **root_table, FILE *input_file, Entry ***all_tables){
     register unsigned char file_buffer = 0;
     register int file_buffer_length = 0;
     register int neededbits = 8;
     register unsigned char buffer = 0;
-    Entry **table = root_table;
+    register uint8_t table_n = 0;
+    Entry *entry;
     while (1)
     {
         // fill buffer from file
@@ -141,24 +147,25 @@ void decode_stream(Entry **root_table, FILE *input_file){
         }
 
         // If there is no nested table, then print a character is associated with the encoding
-        if (table[buffer]->innertable == NULL)
+        entry = all_tables[table_n][buffer];
+        if (entry->c != 0)
         {
-            if (table[buffer]->c == 26)
+            if (entry->c == 26)
             {
                 fprintf(stderr, "EOT\n");
                 break;
             }
             // Get code length from lookup table entry
-            neededbits = table[buffer]->code_length;
+            neededbits = entry->code_length_table_num;
             // print the character associated with the encoding
-            putc(table[buffer]->c,stdout);
-            table = root_table;
+            putc(entry->c,stdout);
+            table_n = 0;
         }
         // If there is a nested table, then set the nested table and retrieve 8 more bits
         // This means that the associated encoding is longer than 8 bits and requires a secondary table
         else
         {
-            table = table[buffer]->innertable;
+            table_n = entry->code_length_table_num;
             neededbits = 8;
         }
 
@@ -174,7 +181,6 @@ int main(int argc, char **argv)
         printf("Usage: ./decode <input_file.txt>\n");
         exit(1);
     }
-
     char *f_name = argv[1];
     FILE *input_file = fopen(f_name, "rb");
     if (input_file == NULL)
@@ -183,8 +189,14 @@ int main(int argc, char **argv)
         exit(1);
     }
 
+    Entry **all_tables[MAX_NUM_TABLES];
+    int total_number_of_tables = 0;
+
     Entry *root_table[256] = {0};
-    build_lookup_table(root_table, input_file);
-    decode_stream(root_table, input_file);
+    all_tables[total_number_of_tables] = root_table;
+    total_number_of_tables++;
+    build_lookup_table(input_file, all_tables, &total_number_of_tables);
+    fprintf(stderr,"Number of tables: %d", total_number_of_tables);
+    decode_stream(root_table, input_file, all_tables);
     fclose(input_file);
 }
